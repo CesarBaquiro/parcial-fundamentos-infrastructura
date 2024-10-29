6ccf6dcc9f2168bd6df79fcda7583c706be6d09a
CesarBaquiro <cmbm.kf@gmail.com>
2024-10-29 03:01:01 -0500

    Update code

--- calculadora.asm
+++ calculadora.asm
@@ -1,117 +1,168 @@
-section .bss
-        buffer resb 128 ; Reserva un buffer de 128 bytes
-        num1 resb 10    ; Buffer para el primer número
-        num2 resb 10    ; Buffer para el segundo número
-        resultado resb 20 ; Buffer para el resultado
-
 section .data
-        mensaje db 'Seleccione una opcion:', 0xA ; Mensaje a mostrar seguido de un salto de línea
 
-	msg_num1 db 'Ingrese el primer numero', 0xA
-	msg_num2 db 'Ingrese el segundo numero', 0xA
 
-        opciones db ' 1. Suma - 2. Resta - 3. Multiplicacion - 4. Division - 5. Residuo de una division - 6. Salir', 0xA ; Opciones
-	msg_suma db '---SUMAR---', 0xA
-        msg_resta db '---RESTAR---', 0xA
-	msg_mult db '---MULTIPLICAR---', 0xA
-        msg_div db '---DIVIDIR---', 0xA
-        msg_mod db '---CALCULAR RESIDUO---', 0xA
-        msg_resultado db 'El resultado es: ', 0xA
-        fin db 0 ; Terminador nulo
+        ; Mensajes
+
+        mensaje            db              10,'Seleccione una opcion:',0
+        lmensaje           equ             $ - mensaje
+
+        msg_num1            db              10,'Numero 1: ',0
+        lmsg_num1           equ             $ - msg_num1
+
+        msg_num2            db              'Numero 2: ',0
+        lmsg_num2           equ             $ - msg_num2
+
+        opciones            db              10,'1. Suma - 2. Resta - 3. Multiplicacion - 4. Division - 5. Residuo de una division - 6. Salir',10,0
+        lopciones           equ             $ - opciones
+
+
+        msg_suma db '---SUMAR---', 10,0
+        lmsg_suma equ $ - msg_suma
+
+        msg_resta db '---RESTAR---', 10,0
+        lmsg_resta equ $ - msg_resta
+        
+        msg_mult db '---MULTIPLICAR---', 10,0
+        lmsg_mult equ $ - msg_mult
+        
+        msg_div db '---DIVIDIR---', 10,0
+        lmsg_div equ $ - msg_div
+        
+        msg_mod db '---CALCULAR RESIDUO---', 10,0
+        lmsg_mod equ $ - msg_mod
+
+        msg_resultado            db              10,'El resultado es: ',0
+        lmsg_resultado           equ             $ - msg_resultado
+
+
+section .bss
+        opcionSeleccionada:         resb    2
+        num1:           resb    2
+        num2:           resb    2
+        resultado:      resb    2
+
 
 section .text
         global _start
 
 _start:
 main_loop:
-        ; Escribir el mensaje en la consola
+        ; Imprimimos en pantalla el mensaje 1
         mov eax, 4
         mov ebx, 1
         mov ecx, mensaje
-        mov edx, 23
-        int 0x80
+        mov edx, lmensaje
+        int 80h
 
-        ; Escribir las opciones en la consola
+        ; Imprimimos en pantalla el mensaje 1
         mov eax, 4
         mov ebx, 1
         mov ecx, opciones
-        mov edx, 94 ; Longitud del mensaje opciones
-        int 0x80
+        mov edx, lopciones
+        int 80h
 
         ; Leer del teclado
-        mov eax, 3
         mov ebx, 0
-        mov ecx, buffer
-        mov edx, 128
-        int 0x80
+        mov ecx, opcionSeleccionada
+        mov edx, 2
+        mov eax, 3
+        int 80h
 
-        ; Comprobar la opción ingresada
-        cmp byte [buffer], '1' ; Suma
+        mov ah, [opcionSeleccionada]        ; Movemos la opcion seleccionada a el registro AH
+        sub ah, '0'             ; Convertimos el valor ingresado de ascii a decimal
+
+        ; Comparamos la opcion seleccionada para saber que operacion realizar.
+        ; JE = Jump if equal = Saltamos si el valor comparado es igual
+
+        cmp ah, 1
         je sumar
-        cmp byte [buffer], '2' ; Resta
+
+        cmp ah, 2
         je restar
-        cmp byte [buffer], '3' ; Multiplicación
+
+        cmp ah, 3
         je multiplicar
-        cmp byte [buffer], '4' ; División
+
+        cmp ah, 4
         je dividir
-        cmp byte [buffer], '5' ; Residuo
+
+        cmp ah, 5
         je residuo
-        cmp byte [buffer], '6' ; Salir
+
+        cmp ah, 6
         je salir
 
         jmp main_loop ; Volver al inicio si la opción no es válida
 
 sumar:
 
-	; Mensaje indicando la operacion a realizar
+        ; Mensaje indicando la operacion a realizar
         mov eax, 4
         mov ebx, 1
         mov ecx, msg_suma
-        mov edx, 12
-        int 0x80
+        mov edx, lmsg_suma
+        int 80h
 
-	; Mensaje para el ingreso del numero 1
+        ; Mensaje para el ingreso del numero 1
         mov eax, 4
         mov ebx, 1
         mov ecx, msg_num1
-        mov edx, 25 ; Longitud del mensaje Ingrese el primer numer
-        int 0x80
+        mov edx, lmsg_num1 ; Longitud del mensaje Ingrese el primer numer
+        int 80h
 
         ; Leer el primer número
         mov eax, 3
         mov ebx, 0
         mov ecx, num1
-        mov edx, 10
-        int 0x80
+        mov edx, 2
+        int 80h
 
-	; Mensaje para el ingreso del numero 2
+        ; Mensaje para el ingreso del numero 2
         mov eax, 4
         mov ebx, 1
         mov ecx, msg_num2
-        mov edx, 26 ; Longitud del mensaje opciones
-        int 0x80
+        mov edx, lmsg_num2 ; Longitud del mensaje opciones
+        int 80h
 
         ; Leer el segundo número
         mov eax, 3
         mov ebx, 0
         mov ecx, num2
-        mov edx, 10
-        int 0x80
+        mov edx, 2
+        int 80h
 
-        ; Convertir los números de texto a enteros
-        mov ecx, num1
-        call str2intNum1
-        mov ebx, eax ; Guardar el primer número en ebx
+                ; Imprimimos en pantalla el mensaje de resultado
+        mov eax, 4
+        mov ebx, 1
+        mov ecx, msg_resultado
+        mov edx, lmsg_resultado
+        int 80h
 
-        ; Convertir el segundo número de texto a entero
-        mov ecx, num2
-        call str2intNum2
-        add ebx, eax ; Sumar el segundo número al primero
+        ; Movemos los numeros ingresados a los registro AL y BL
+        mov al, [num1]
+        mov bl, [num2]
 
-        ; Imprimir el resultado
-        call print_result
+        ; Convertimos los valores ingresados de ascii a decimal
+        sub al, '0'
+        sub bl, '0'
 
-	jmp main_loop
+        ; Sumamos el registro AL y BL
+        add al, bl
+
+        ; Convertimos el resultado de la suma de decimal a ascii
+        add al, '0'
+
+        ; Movemos el resultado a un espacio reservado en la memoria
+        mov [resultado], al
+
+        ; Imprimimos en pantalla el resultado
+        mov eax, 4
+        mov ebx, 1
+        mov ecx, resultado
+        mov edx, 2
+        int 80h
+
+        jmp main_loop
 
 restar:
 
@@ -119,50 +170,68 @@ restar:
         mov eax, 4
         mov ebx, 1
         mov ecx, msg_resta
-        mov edx, 13
-        int 0x80
+        mov edx, lmsg_resta
+        int 80h
 
-        ; Mensaje para el ingreso del numero 1
+       ; Mensaje para el ingreso del numero 1
         mov eax, 4
         mov ebx, 1
         mov ecx, msg_num1
-        mov edx, 25 ; Longitud del mensaje Ingrese el primer numer
-        int 0x80
+        mov edx, lmsg_num1 ; Longitud del mensaje Ingrese el primer numer
+        int 80h
 
         ; Leer el primer número
         mov eax, 3
         mov ebx, 0
         mov ecx, num1
-        mov edx, 10
-        int 0x80
+        mov edx, 2
+        int 80h
 
         ; Mensaje para el ingreso del numero 2
         mov eax, 4
         mov ebx, 1
         mov ecx, msg_num2
-        mov edx, 26 ; Longitud del mensaje opciones
-        int 0x80
-
+        mov edx, lmsg_num2 ; Longitud del mensaje opciones
+        int 80h
 
         ; Leer el segundo número
         mov eax, 3
         mov ebx, 0
         mov ecx, num2
-        mov edx, 10
-        int 0x80
+        mov edx, 2
+        int 80h
 
-        ; Convertir los números de texto a enteros
-        mov ecx, num1
-        call str2intNum1
-        mov ebx, eax ; Guardar el primer número en ebx
+                ; Imprimimos en pantalla el mensaje de resultado
+        mov eax, 4
+        mov ebx, 1
+        mov ecx, msg_resultado
+        mov edx, lmsg_resultado
+        int 80h
 
-        ; Convertir el segundo número de texto a entero
-        mov ecx, num2
-        call str2intNum2
-        add ebx, eax ; Sumar el segundo número al primero
+         ; Movemos los numeros ingresados a los registro AL y BL
+        mov al, [num1]
+        mov bl, [num2]
 
-        ; Imprimir el resultado
-        call print_result
+        ; Convertimos los valores ingresados de ascii a decimal
+        sub al, '0'
+        sub bl, '0'
+
+        ; Restamos el registro AL y BL
+        sub al, bl
+
+        ; Convertimos el resultado de la resta de decimal a ascii
+        add al, '0'
+
+        ; Movemos el resultado a un espacio reservado en la memoria
+        mov [resultado], al
+
+                ; Imprimimos en pantalla el resultado
+        mov eax, 4
+        mov ebx, 1
+        mov ecx, resultado
+        mov edx, 1
+        int 80h
+
         jmp main_loop
 
 multiplicar:
@@ -171,49 +240,68 @@ multiplicar:
         mov eax, 4
         mov ebx, 1
         mov ecx, msg_mult
-        mov edx, 18
-        int 0x80
+        mov edx, lmsg_mult
+        int 80h
 
         ; Mensaje para el ingreso del numero 1
         mov eax, 4
         mov ebx, 1
         mov ecx, msg_num1
-        mov edx, 25 ; Longitud del mensaje Ingrese el primer numer
-        int 0x80
+        mov edx, lmsg_num1 ; Longitud del mensaje Ingrese el primer numer
+        int 80h
 
         ; Leer el primer número
         mov eax, 3
         mov ebx, 0
         mov ecx, num1
-        mov edx, 10
-        int 0x80
+        mov edx, 2
+        int 80h
 
         ; Mensaje para el ingreso del numero 2
         mov eax, 4
         mov ebx, 1
         mov ecx, msg_num2
-        mov edx, 26 ; Longitud del mensaje opciones
-        int 0x80
+        mov edx, lmsg_num2 ; Longitud del mensaje opciones
+        int 80h
 
         ; Leer el segundo número
         mov eax, 3
         mov ebx, 0
         mov ecx, num2
-        mov edx, 10
-        int 0x80
+        mov edx, 2
+        int 80h
 
-        ; Convertir los números de texto a enteros
-        mov ecx, num1
-        call str2intNum1
-        mov ebx, eax ; Guardar el primer número en ebx
+         ; Imprimimos en pantalla el mensaje de resultado
+        mov eax, 4
+        mov ebx, 1
+        mov ecx, msg_resultado
+        mov edx, lmsg_resultado
+        int 80h
 
-        ; Convertir el segundo número de texto a entero
-        mov ecx, num2
-        call str2intNum2
-        add ebx, eax ; Sumar el segundo número al primero
+               ; Movemos los numeros ingresados a los registro AL y BL
+        mov al, [num1]
+        mov bl, [num2]
 
-        ; Imprimir el resultado
-        call print_result
+        ; Convertimos los valores ingresados de ascii a decimal
+        sub al, '0'
+        sub bl, '0'
+
+        ; Multiplicamos. AX = AL X BL
+        mul bl
+
+        ; Convertimos el resultado de la resta de decimal a ascii
+        add ax, '0'
+
+        ; Movemos el resultado a un espacio reservado en la memoria
+        mov [resultado], ax
+
+        ; Imprimimos en pantalla el resultado
+        mov eax, 4
+        mov ebx, 1
+        mov ecx, resultado
+        mov edx, 1
+        int 80h
+
         jmp main_loop
 
 dividir:
@@ -222,49 +310,72 @@ dividir:
         mov eax, 4
         mov ebx, 1
         mov ecx, msg_div
-        mov edx, 14
-        int 0x80
+        mov edx, lmsg_div
+        int 80h
 
         ; Mensaje para el ingreso del numero 1
         mov eax, 4
         mov ebx, 1
         mov ecx, msg_num1
-        mov edx, 25 ; Longitud del mensaje Ingrese el primer numer
-        int 0x80
+        mov edx, lmsg_num1 ; Longitud del mensaje Ingrese el primer numer
+        int 80h
 
         ; Leer el primer número
         mov eax, 3
         mov ebx, 0
         mov ecx, num1
-        mov edx, 10
-        int 0x80
+        mov edx, 2
+        int 80h
 
         ; Mensaje para el ingreso del numero 2
         mov eax, 4
         mov ebx, 1
         mov ecx, msg_num2
-        mov edx, 26 ; Longitud del mensaje opciones
-        int 0x80
+        mov edx, lmsg_num2 ; Longitud del mensaje opciones
+        int 80h
 
         ; Leer el segundo número
         mov eax, 3
         mov ebx, 0
         mov ecx, num2
-        mov edx, 10
-        int 0x80
+        mov edx, 2
+        int 80h
 
-        ; Convertir los números de texto a enteros
-        mov ecx, num1
-        call str2intNum1
-        mov ebx, eax ; Guardar el primer número en ebx
+         ; Imprimimos en pantalla el mensaje de resultado
+        mov eax, 4
+        mov ebx, 1
+        mov ecx, msg_resultado
+        mov edx, lmsg_resultado
+        int 80h
 
-        ; Convertir el segundo número de texto a entero
-        mov ecx, num2
-        call str2intNum2
-        add ebx, eax ; Sumar el segundo número al primero
+               ; Movemos los numeros ingresados a los registro AL y BL
+        mov al, [num1]
+        mov bl, [num2]
 
-        ; Imprimir el resultado
-        call print_result
+        ; Igualamos a cero los registros DX y AH
+        mov dx, 0
+        mov ah, 0
+
+        ; Convertimos los valores ingresados de ascii a decimal
+        sub al, '0'
+        sub bl, '0'
+
+        ; Division. AL = AX / BL. AX = AH:AL
+        div bl
+
+        ; Convertimos el resultado de la resta de decimal a ascii
+        add ax, '0'
+
+        ; Movemos el resultado a un espacio reservado en la memoria
+        mov [resultado], ax
+
+        ; Imprimimos en pantalla el resultado
+        mov eax, 4
+        mov ebx, 1
+        mov ecx, resultado
+        mov edx, 1
+        int 80h
+
         jmp main_loop
 
 residuo:
@@ -273,136 +384,74 @@ residuo:
         mov eax, 4
         mov ebx, 1
         mov ecx, msg_mod
-        mov edx, 23
-        int 0x80
+        mov edx, lmsg_mod
+        int 80h
 
         ; Mensaje para el ingreso del numero 1
         mov eax, 4
         mov ebx, 1
         mov ecx, msg_num1
-        mov edx, 25 ; Longitud del mensaje Ingrese el primer numer
-        int 0x80
+        mov edx, lmsg_num1 ; Longitud del mensaje Ingrese el primer numer
+        int 80h
 
         ; Leer el primer número
         mov eax, 3
         mov ebx, 0
         mov ecx, num1
-        mov edx, 10
-        int 0x80
+        mov edx, 2
+        int 80h
 
         ; Mensaje para el ingreso del numero 2
         mov eax, 4
         mov ebx, 1
         mov ecx, msg_num2
-        mov edx, 26 ; Longitud del mensaje opciones
-        int 0x80
+        mov edx, lmsg_num2 ; Longitud del mensaje opciones
+        int 80h
 
-       ; Leer el segundo número
+        ; Leer el segundo número
         mov eax, 3
         mov ebx, 0
         mov ecx, num2
-        mov edx, 10
-        int 0x80
+        mov edx, 2
+        int 80h
 
-        ; Convertir los números de texto a enteros
-        mov ecx, num1
-        call str2intNum1
-        mov ebx, eax ; Guardar el primer número en ebx
+                ; Imprimimos en pantalla el mensaje de resultado
+        mov eax, 4
+        mov ebx, 1
+        mov ecx, msg_resultado
+        mov edx, lmsg_resultado
+        int 80h
 
-        ; Convertir el segundo número de texto a entero
-        mov ecx, num2
-        call str2intNum2
-        add ebx, eax ; Sumar el segundo número al primero
+         ; Movemos los numeros ingresados a los registro AL y BL
+        mov al, [num1]
+        mov bl, [num2]
 
-        ; Imprimir el resultado
-        mov eax, edx
-        call print_result
+        ; Convertimos los valores ingresados de ascii a decimal
+        sub al, '0'
+        sub bl, '0'
+
+        ; Sumamos el registro AL y BL
+        add al, bl
+
+        ; Convertimos el resultado de la suma de decimal a ascii
+        add al, '0'
+
+        ; Movemos el resultado a un espacio reservado en la memoria
+        mov [resultado], al
+
+        ; Imprimimos en pantalla el resultado
+        mov eax, 4
+        mov ebx, 1
+        mov ecx, resultado
+        mov edx, 2
+        int 80h
+
         jmp main_loop
 
 salir:
         ; Salir del programa
         mov eax, 1
         xor ebx, ebx
-        int 0x80
-
-
-; Función para convertir una cadena a un entero
-str2intNum1:
-        mov eax, 0
-        mov ecx, 10
-        mov esi, num1 ; Dirección del buffer que contiene el número
-        next_char1:
-                movzx edx, byte [esi]
-                cmp edx, '0'
-                jl end_str2intNum1
-                cmp edx, '9'
-                jg end_str2intNum1
-                sub edx, '0'
-                imul eax, ecx
-                add eax, edx
-                inc esi
-                jmp next_char1
-        end_str2intNum1:
-                ret
-
-
-; Función para convertir una cadena a un entero
-str2intNum2:
-        mov eax, 0
-        mov ecx, 10
-        mov esi, num2 ; Dirección del buffer que contiene el número 2
-        next_char2:
-                movzx edx, byte [esi]
-                cmp edx, '0'
-                jl end_str2intNum2
-                cmp edx, '9'
-                jg end_str2intNum2
-                sub edx, '0'
-                imul eax, ecx
-                add eax, edx
-                inc esi
-                jmp next_char2
-        end_str2intNum2:
-                ret
-
-
-; Función para convertir un entero a una cadena
-int2str:
-        mov ecx, resultado ; Dirección del buffer que contiene el resultado
-        mov ebx, eax ; Valor a convertir
-        mov eax, 0
-        next_digit:
-                xor edx, edx
-                div ecx
-                add dl, '0'
-                dec ecx
-                mov [ecx], dl
-                test ebx, ebx
-                jnz next_digit
-                ret
-
-
-print_result:
-        ; Imprimir el mensaje de resultado
-        mov eax, 4
-        mov ebx, 1
-        mov ecx, msg_resultado
-        mov edx, 16
-        int 0x80
-
-        ; Convertir el resultado a cadena y mostrar
-        mov eax, ebx ; El resultado está en ebx
-        call int2str ; Convertir el entero a cadena
-
-        ; Imprimir la cadena resultante
-        mov eax, 4
-        mov ebx, 1
-        mov ecx, resultado ; Dirección del buffer que contiene el resultado
-        mov edx, 128       ; Longitud máxima del resultado
-        int 0x80
-
-        ret
-
-
+        int 80h
 
 
